ea67e26ca3fcb5476615d9ab571af536cf4f6917
b1ruk <biruk3870@gmail.com>
2024-07-31 01:47:59 -0300

    application creation

--- src/app/feature-modules/customer/customer-form/customer-form.component.ts
+++ src/app/feature-modules/customer/customer-form/customer-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ToastrService} from "ngx-toastr";
+import {OnboardingServiceService} from "../../../api/service/onboarding-service.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-customer-form',
@@ -6,14 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-form.component.scss']
 })
 export class CustomerFormComponent implements OnInit {
+  public applicationForm: FormGroup;
 
-  constructor() { }
+
+  constructor(
+    private fb: FormBuilder,
+    public toastrService: ToastrService,
+    public onboardingService: OnboardingServiceService, private route: ActivatedRoute
+  ) {
+    this.applicationForm = this.fb.group({
+      id: [null],
+      purpose: ['', Validators.required],
+      companyName: ['', Validators.required],
+      entityType: ['', Validators.required],
+      businessActivity: ['', Validators.required],
+      countryOfIncorporation: ['', Validators.required],
+      registrationNumber: ['', Validators.required],
+      dateOfIncorporation: ['', Validators.required],
+      directorName: ['', Validators.required],
+      passportNumber: ['', Validators.required],
+      applicantName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+
+  }
 
   ngOnInit(): void {
   }
 
-  openCamera(){
+  onApplicationSubmit(){
+    this.onboardingService.createApplication({
+      ...this.applicationForm.value
+    })
+      .subscribe(resp => {
+        if (resp) {
+          this.toastrService.success('Created','Application');
+        }
+      })
 
   }
+
+
 
 }
